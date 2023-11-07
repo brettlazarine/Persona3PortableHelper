bc5e9cdaeef3d69474fc5d384301dcb674c14a5f
Brett Lazarine <jblazarine@gmail.com>
2023-11-06 22:35:36 -0600

    create SocialLinksView

--- P3PHelper/P3PHelper/App.xaml.cs
+++ P3PHelper/P3PHelper/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace P3PHelper;
+﻿using P3PHelper.MVVM.Views;
+
+namespace P3PHelper;
 
 public partial class App : Application
 {
@@ -6,6 +8,6 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-		MainPage = new AppShell();
+		MainPage = new SocialLinksView();
 	}
 }
---
+++ P3PHelper/P3PHelper/MVVM/Views/SocialLinksView.xaml.cs
@@ -0,0 +1,9 @@
+namespace P3PHelper.MVVM.Views;
+
+public partial class SocialLinksView : ContentPage
+{
+	public SocialLinksView()
+	{
+		InitializeComponent();
+	}
+}
