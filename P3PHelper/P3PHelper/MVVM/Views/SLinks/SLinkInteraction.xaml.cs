b4ac5663f9a52829c7607451d1ae8f4113c795c1
Brett Lazarine <jblazarine@gmail.com>
2023-11-14 12:11:01 -0600

    complete male properties/values labels and arrow gesture recognizers

--- P3PHelper/P3PHelper/MVVM/Views/SLinks/SLinkInteraction.xaml.cs
+++ P3PHelper/P3PHelper/MVVM/Views/SLinks/SLinkInteraction.xaml.cs
@@ -19,4 +19,26 @@ public partial class SLinkInteraction : ContentPage
 
         BindingContext = sLink;
     }
+
+    private void MaleDate_Tapped(object sender, TappedEventArgs e)
+    {
+        var arrow = sender as Image;
+        
+        MaleDate.IsVisible = !MaleDate.IsVisible;
+        arrow.Rotation = MaleDate.IsVisible ? 180 : 0;
+    }
+    private void MaleHow_Tapped(object sender, TappedEventArgs e)
+    {
+        var arrow = sender as Image;
+
+        MaleHow.IsVisible = !MaleHow.IsVisible;
+        arrow.Rotation = MaleHow.IsVisible ? 180 : 0;
+    }
+    private void MaleAvailability_Tapped(object sender, TappedEventArgs e)
+    {
+        var arrow = sender as Image;
+
+        MaleAvailability.IsVisible = !MaleAvailability.IsVisible;
+        arrow.Rotation = MaleAvailability.IsVisible ? 180 : 0;
+    }
 }
