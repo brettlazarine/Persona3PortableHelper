df8ac7224042daff9b60e1b556b0863d87bf38af
Brett Lazarine <jblazarine@gmail.com>
2023-11-18 08:14:38 -0600

    update repo

--- P3PHelper/P3PHelper/obj/Debug/net7.0-android/P3PHelper.AssemblyInfo.cs
+++ P3PHelper/P3PHelper/obj/Debug/net7.0-android/P3PHelper.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P3PHelper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4eafe40b0a7926f75c4ed147b829a7d98050dc73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+2e97df3c5a97cc54d33323f21c81cd1d15ae34c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("P3PHelper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P3PHelper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
