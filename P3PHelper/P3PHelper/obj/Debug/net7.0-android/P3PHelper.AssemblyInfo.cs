e6fd2be69d70fb84a5890b38971c1d629f4a886b
Brett Lazarine <jblazarine@gmail.com>
2023-11-20 14:05:38 -0600

    adjust appwide styles for shell, titleview

--- P3PHelper/P3PHelper/obj/Debug/net7.0-android/P3PHelper.AssemblyInfo.cs
+++ P3PHelper/P3PHelper/obj/Debug/net7.0-android/P3PHelper.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P3PHelper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+9b8e16f8e5111c6dedeed2753c72f9d82d7c75be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("P3PHelper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P3PHelper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
