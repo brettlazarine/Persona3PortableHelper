d0a969c606911e52078b32da62c71a4d2e88690c
Brett Lazarine <jblazarine@gmail.com>
2023-12-02 19:56:36 -0600

    create Requests class and interface

--- P3PHelper/P3PHelper/obj/Debug/net7.0/P3PHelper.AssemblyInfo.cs
+++ P3PHelper/P3PHelper/obj/Debug/net7.0/P3PHelper.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P3PHelper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+207a3690ed9065aac55724019c20f510e5bbba0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("P3PHelper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P3PHelper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
