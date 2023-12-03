f01945018a3bbcf29f57e621a8aa2e344228fe48
Brett Lazarine <jblazarine@gmail.com>
2023-12-02 19:22:28 -0600

    finish tests for SLink portion of app

---
+++ P3PHelper/UnitTests/InteractionStoryTests.cs
@@ -0,0 +1,30 @@
+﻿namespace UnitTests
+{
+    public class InteractionStoryTests
+    {
+        [Fact]
+        public void ScrollRanks_Test()
+        {
+            //Arrange
+            var vm = new InteractionStoryViewModel();
+            //Act
+            var count = vm.ScrollRanks.Count;
+            //Assert
+            Assert.Equal(3, count);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(1000)]
+        public void AdjustY_Test(int y)
+        {
+            //Arrange
+            var vm = new InteractionStoryViewModel();
+            //Act
+            var newY = vm.AdjustY(y);
+            //Assert
+            Assert.Equal(y + 1000, newY);
+        }
+    }
+}
