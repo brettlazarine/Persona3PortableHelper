93c8f34127680005de6e8069feef21c2a18bc2ed
Brett Lazarine <jblazarine@gmail.com>
2023-12-05 01:41:13 -0600

    complete onetotwenty view, begin checkbox event handler

--- P3PHelper/P3PHelper/MVVM/Views/Requests/OneToTwentyView.xaml.cs
+++ P3PHelper/P3PHelper/MVVM/Views/Requests/OneToTwentyView.xaml.cs
@@ -4,10 +4,15 @@ namespace P3PHelper.MVVM.Views.Requests;
 
 public partial class OneToTwentyView : ContentPage
 {
-	public RequestsViewModel Vm = new RequestsViewModel();
+	public RequestsViewModel Vm = new();
 	public OneToTwentyView()
 	{
 		InitializeComponent();
 		BindingContext = Vm;
 	}
+
+    private void CheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+
+    }
 }
