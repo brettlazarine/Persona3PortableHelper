8a3ed79e54cf1d27a6082a018aea0654a3b5b822
Brett Lazarine <jblazarine@gmail.com>
2023-12-31 03:42:47 -0600

    Complete Lovers
    Add Lovers to db, verify

--- P3PHelper/P3PHelper/obj/Debug/net7.0-android/P3PHelper.AssemblyInfo.cs
+++ P3PHelper/P3PHelper/obj/Debug/net7.0-android/P3PHelper.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P3PHelper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+5d3fc23a722eac74cfeb7d518872586522ed1cc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b3d6c04b528b6f9d35d4145fda7e76e76c9b503f")]
 [assembly: System.Reflection.AssemblyProductAttribute("P3PHelper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P3PHelper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
