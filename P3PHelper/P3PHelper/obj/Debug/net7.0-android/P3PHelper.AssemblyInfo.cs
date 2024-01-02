a3fcc57a442331d5fa1a7302cd427f41c98c8aab
Brett Lazarine <jblazarine@gmail.com>
2024-01-02 01:49:42 -0600

    Complete Strength
    Add Strength to db, verify

--- P3PHelper/P3PHelper/obj/Debug/net7.0-android/P3PHelper.AssemblyInfo.cs
+++ P3PHelper/P3PHelper/obj/Debug/net7.0-android/P3PHelper.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P3PHelper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f177f12ca227fded98b3e8bf40c5eea0dc0efe39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0cf2c8f8e9fbf470170bf9a7d6c00d7c1634ee4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("P3PHelper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P3PHelper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
