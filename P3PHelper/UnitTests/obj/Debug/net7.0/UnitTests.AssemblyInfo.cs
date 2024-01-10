c974bd695a8ed4ffa12a38fdcfa22cc8a89f1324
Brett Lazarine <jblazarine@gmail.com>
2024-01-10 13:27:05 -0600

    Update visual studio

--- P3PHelper/UnitTests/obj/Debug/net7.0/UnitTests.AssemblyInfo.cs
+++ P3PHelper/UnitTests/obj/Debug/net7.0/UnitTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b3d043e4d47d5a182321201f33728ead27a745a")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
