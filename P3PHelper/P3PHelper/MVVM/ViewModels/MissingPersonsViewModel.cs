d4c66d2a879a71a4bee117091b8cfeffe1f5ca46
Brett Lazarine <jblazarine@gmail.com>
2024-01-13 13:39:38 -0600

    MissingPersonsView
    Complete the MissingPersonsView and codebehind

--- P3PHelper/P3PHelper/MVVM/ViewModels/MissingPersonsViewModel.cs
+++ P3PHelper/P3PHelper/MVVM/ViewModels/MissingPersonsViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using P3PHelper.MVVM.Models;
+using P3PHelper.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,16 @@ namespace P3PHelper.MVVM.ViewModels
 {
     public class MissingPersonsViewModel
     {
+        public List<MissingPerson> MissingPeople { get; set; } = new();
+        public ProgressRepository ProgressRepo { get; set; } = new();
 
+        public MissingPersonsViewModel()
+        {
+            foreach (var person in ProgressRepo.GetMissingPersons())
+            {
+                person.Info = person.Info.Replace("\\n", Environment.NewLine);
+                MissingPeople.Add(person);
+            }
+        }
     }
 }
--- P3PHelper/P3PHelper/MVVM/Views/MissingPersons/MissingPersonsView.xaml.cs
+++ P3PHelper/P3PHelper/MVVM/Views/MissingPersons/MissingPersonsView.xaml.cs
@@ -1,9 +1,65 @@
+using P3PHelper.MVVM.Models;
+using P3PHelper.MVVM.ViewModels;
+using P3PHelper.Repositories;
+using System.Diagnostics;
+
 namespace P3PHelper.MVVM.Views.MissingPersons;
 
 public partial class MissingPersonsView : ContentPage
 {
+	MissingPersonsViewModel viewModel = new();
+    ProgressRepository ProgressRepo = new();
 	public MissingPersonsView()
 	{
 		InitializeComponent();
+		BindingContext = viewModel;
 	}
+
+    private void MissingPersonCheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        if (sender is not CheckBox checkBox)
+        {
+            Debug.WriteLine("*** Unexpected sender type in CheckBox_CheckedChanged ***");
+            return;
+        }
+        if (checkBox.BindingContext is not MissingPerson missingPerson)
+        {
+            Debug.WriteLine("*** Unexpected BindingContext type in RequestCheckBox_CheckedChanged ***");
+            return;
+        }
+        // Toggle the IsVisible property of the MissingPersonInfo Grid
+        try
+        {
+            var parent = checkBox.Parent as Layout;
+            if (parent == null)
+            {
+                Debug.WriteLine("*** Parent is null in RequestCheckBox_CheckedChanged ***");
+                return;
+            }
+
+            var hider = parent.Children
+                .OfType<Grid>()
+                .FirstOrDefault(x => x.AutomationId == "MissingPersonInfo");
+            if (hider != null)
+            {
+                hider.IsVisible = !hider.IsVisible;
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine("*** " + ex.Message + " ***");
+            DisplayAlert("Error", "Error handling CheckBox tap", "OK");
+        }
+        // Update the MissingPerson in the database
+        try
+        {
+            int isCompleted = checkBox.IsChecked ? 1 : 0;
+            ProgressRepo.UpdateMissingPerson(missingPerson.Id, isCompleted);
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine("*** " + ex.Message + " ***");
+            DisplayAlert("Error", "Error updating Missing Person", "OK");
+        }
+    }
 }
