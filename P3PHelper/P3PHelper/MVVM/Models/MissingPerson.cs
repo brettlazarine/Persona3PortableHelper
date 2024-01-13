22b041c53d15174ae6d13930e9a3bff3abf14fa0
Brett Lazarine <jblazarine@gmail.com>
2024-01-13 09:02:41 -0600

    MissingPerson backend
    Create MissingPerson interface and model, add data to MissingPerson table in db

---
+++ P3PHelper/P3PHelper/MVVM/Models/MissingPerson.cs
@@ -0,0 +1,23 @@
+﻿using P3PHelper.Abstractions;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3PHelper.MVVM.Models
+{
+    public class MissingPerson : IMissingPerson
+    {
+        [PrimaryKey, Indexed]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Available { get; set; }
+        public string Deadline { get; set; }
+        public string Info { get; set; }
+        public string Reward { get; set; }
+        public int IsCompleted { get; set; }
+    }
+}
