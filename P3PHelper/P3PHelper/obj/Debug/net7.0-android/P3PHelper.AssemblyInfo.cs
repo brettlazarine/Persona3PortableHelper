b9d0da16cddec31c30b3ed0e4b156c8f508ac6fc
Brett Lazarine <jblazarine@gmail.com>
2024-01-14 16:34:56 -0600

    MAUI ToolKit package

--- P3PHelper/P3PHelper/obj/Debug/net7.0-android/P3PHelper.AssemblyInfo.cs
+++ P3PHelper/P3PHelper/obj/Debug/net7.0-android/P3PHelper.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P3PHelper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a340a2cd4118d07950806c4c186e3124dfd3a486")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+1af9ac2d8a4a5a3259881e64242274bf8524aa56")]
 [assembly: System.Reflection.AssemblyProductAttribute("P3PHelper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P3PHelper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
