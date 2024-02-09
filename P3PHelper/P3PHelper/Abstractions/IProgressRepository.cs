823a14a7c55c5d2561eecb56d75b68ab546a8058
Brett Lazarine <jblazarine@gmail.com>
2024-02-09 06:18:30 -0600

    update repo, closes #62, closes #36, closes #60
    Moved all controls from CBs to VMs, added async repo methods, eliminated automationIDs

---
+++ P3PHelper/P3PHelper/Abstractions/IProgressRepository.cs
@@ -0,0 +1,44 @@
+﻿using P3PHelper.MVVM.Models;
+
+namespace P3PHelper.Abstractions
+{
+    public interface IProgressRepository
+    {
+        List<SLink> GetSLinks();
+        Task<List<SLink>> GetSLinksAsync();
+        SLink GetSLink(string arcanaName);
+        Task<SLink> GetSLinkAsync(string arcanaName);
+
+
+        List<RankUp> GetRankUps();
+        Task<List<RankUp>> GetRankUpsAsync();
+        List<RankUp> GetRankUp(string arcana);
+        Task<List<RankUp>> GetRankUpAsync(string arcana);
+        void UpdateRankUp(int id, int isCompleted);
+        Task<int> UpdateRankUpAsync(int id, int isCompleted);
+
+
+        List<Request> GetRequests();
+        Task<List<Request>> GetRequestsAsync();
+        Request GetRequest(int questNumber);
+        Task<Request> GetRequestAsync(int questNumber);
+        void UpdateRequest(int questNumber, int isCompleted);
+        Task<int> UpdateRequestAsync(int questNumber, int isCompleted);
+
+
+        List<MissingPerson> GetMissingPersons();
+        Task<List<MissingPerson>> GetMissingPersonsAsync();
+        MissingPerson GetMissingPerson(int id);
+        Task<MissingPerson> GetMissingPersonAsync(int id);
+        void UpdateMissingPerson(int id, int isCompleted);
+        Task<int> UpdateMissingPersonAsync(int id, int isCompleted);
+
+
+        List<SchoolQuestion> GetSchoolQuestions();
+        Task<List<SchoolQuestion>> GetSchoolQuestionsAsync();
+        SchoolQuestion GetSchoolQuestion(int id);
+        Task<SchoolQuestion> GetSchoolQuestionAsync(int id);
+        void UpdateSchoolQuestion(int id, int isCompleted);
+        Task<int> UpdateSchoolQuestionAsync(int id, int isCompleted);
+    }
+}
